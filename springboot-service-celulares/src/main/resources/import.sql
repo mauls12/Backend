8f9e311a86bc1f72fa0e665820996309b0f6b227
mauls12 <123897163+mauls12@users.noreply.github.com>
2024-03-21 21:22:17 -0600

    TAREA 1

---
+++ springboot-service-celulares/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO celulares (name, marca, created_at) VALUES ('S21','Samsung', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('S22','Samsung', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('S23','Samsung', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Iphone 14','Apple', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Iphone 15','Apple', NOW());
+INSERT INTO celulares (name, marca, created_at) VALUES ('Mi 11','Xiaomi', NOW());
